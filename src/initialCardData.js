fb3f710948c0eecc50f7bc4f7af30a8ef04897e1
Silas Reinagel <silas.reinagel@carvana.com>
2022-09-07 09:15:42 -0700

    Updated Default Infographic

--- src/initialCardData.js
+++ src/initialCardData.js
@@ -27,6 +27,57 @@ const createBasicData = () => ({
   altPhoto: 'https://picsum.photos/id/13/800/450',
 });
 
+const createSampleBasicData = () => JSON.parse(`{
+  "styling": {
+    "colors": {
+      "backgroundColor": "#00acd8"
+    }
+  },
+  "basicData": {
+    "title": "Infographics For Next-Gen Communication",
+    "link": {
+      "title": "Try It Yourself - https://liteinfographicmaker.app",
+      "url": "https://liteinfographicmaker.app"
+    },
+    "mainPhoto": "/img/use-infographics.jpg",
+    "altPhoto": "/img/lite-infographic-maker.jpg"
+  },
+  "coreFacts": [
+    {
+      "title": "Visually-Centered",
+      "text": "90% of the information processed by the brain is visual"
+    },
+    {
+      "title": "Fast Communication",
+      "text": "The human brain processes images 60,000 times faster than text"
+    },
+    {
+      "title": "Adaptive Depth",
+      "text": "Unlike just an image, providing text + images enables viewers to go deeper"
+    }
+  ],
+  "detail1": {
+    "title": "Why an Infographic?",
+    "items": [
+      "An infographic puts all the key information in a single view",
+      "An infographic forces the communicator to clarify their key thoughts",
+      "An infographic can be absorbed faster than a video",
+      "An infographic can provide actionable and informative knowledge",
+      "An infographic is highly portable and perfect for chat and social tools"
+    ]
+  },
+  "detail2": {
+    "title": "The Ideal Infographic",
+    "items": [
+      "Leads with your key idea in the title",
+      "Offers a web link to an interactive demo or more detail",
+      "Presents facts that contextualize and strengthen the info",
+      "Shares sets of details that are terse, crisp, and easy to read",
+      "Is easy to create in just minutes"
+    ]
+  }
+}`);
+
 export const createNew = () => 
 ({
   styling: createStyling(),
@@ -36,4 +87,4 @@ export const createNew = () =>
   detail2: createDetail(),
 });
 
-export default createNew;
+export default createSampleBasicData;
